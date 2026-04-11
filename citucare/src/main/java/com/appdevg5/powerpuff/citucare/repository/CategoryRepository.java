f3b989ea2fff985ce05f510a987743bd91651954
LAB24_USER ON WS39 <L24Y09W39@LabHCI.net>
2026-04-11 09:27:15 +0800

    code refactored and applied vertical slicing

--- citucare/src/main/java/com/appdevg5/powerpuff/citucare/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.appdevg5.powerpuff.citucare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.appdevg5.powerpuff.citucare.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- citucare/src/main/java/com/appdevg5/powerpuff/citucare/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.appdevg5.powerpuff.citucare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.appdevg5.powerpuff.citucare.entity.Department;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
