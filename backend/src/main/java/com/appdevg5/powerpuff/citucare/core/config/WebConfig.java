8c3414f5005964a7c821bf8f040541e8a33494f8
Jeff <jefflloyd.seloterio@gmail.com>
2026-05-09 02:01:43 +0800

    added vercel url

--- backend/src/main/java/com/appdevg5/powerpuff/citucare/core/config/WebConfig.java
+++ backend/src/main/java/com/appdevg5/powerpuff/citucare/core/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000",
                                         "http://localhost:5173",
-                                        "https://YOUR-VERCEL-APP.vercel.app"
+                                        "https://citucare-frontend.vercel.app/"
                                     )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
